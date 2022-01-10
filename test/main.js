f23054665bebcea599afa7115b4865d414a39ba8
DDU <ddu6@protonmail.com>
2022-01-10 16:44:15 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,8 +1,8 @@
-const {CLIT}=require('../dist/mod')
-const clit=new CLIT(__dirname)
-;(async ()=>{
+const {CLIT} = require('../dist/mod')
+const clit = new CLIT(__dirname);
+(async () => {
     console.log(await clit.request('https://en.wikipedia.org/wiki/Main_Page'))
-    clit.out(await clit.download('https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4','flower.mp4',{verbose:true}))
+    clit.out(await clit.download('https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4', 'flower.mp4', {verbose: true}))
     clit.out(await clit.existsURL('https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4'))
     clit.out(await clit.existsURL('https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower2.mp4'))
 })()
